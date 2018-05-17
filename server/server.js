1b2173dcbf07c9cf44da5ea215c0b575a40f9ccc
Kashish Kohli <kashish.kohli@ymail.com>
2018-05-17 17:51:35 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const express = require('express');
 const hbs = require('hbs');
@@ -7,6 +8,6 @@ const hbs = require('hbs');
 let app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-	console.log('Starting the server on port 3000');
+app.listen(port, () => {
+	console.log(`Starting the server on port ${port}`);
 });
