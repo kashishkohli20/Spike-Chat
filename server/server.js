be085a5529b6317037c772fa6f05b196d4daa2b9
Kashish Kohli <kashish.kohli@ymail.com>
2018-05-22 04:02:19 +0530

    Sending messages to only people in the same room

--- server/server.js
+++ server/server.js
@@ -44,14 +44,21 @@ io.on('connection', (socket) => {			//server
 	});
 
 	socket.on('createMessage', (message, callback) => {		//server
-		console.log('New Message', message);
-		io.emit('newMessage', generateMessage(message.from, message.text));//final way to emit messages-correct
-		callback('This is from the server');																	//client
+		let user = users.getUser(socket.id);
+
+		if (user && isRealString(message.text)) {
+			io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));//final way to emit messages-correct
+		}
+		callback();																	//client
 
 	});
 
 	socket.on('createLocationMessage', (coords) => {
-		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+		let user = users.getUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+		}
 	});
 });
 
